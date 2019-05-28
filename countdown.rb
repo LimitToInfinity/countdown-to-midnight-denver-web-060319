67f1fd0d2e4ed86a66858d499df9c786de5f0d7a
Ahmed Gaber <21stlimitless@gmail.com>
2019-05-28 19:57:15 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,3 +9,16 @@ def countdown(integer)
   return "HAPPY NEW YEAR!"
 
 end
+
+def countdown_with_sleep(integer)
+  integer = 10
+  
+  while integer >= 1
+    sleep(1)
+    puts "#{integer} SECOND(S)!"
+    integer -= 1
+  end
+  
+  return "HAPPY NEW YEAR!"
+
+end
